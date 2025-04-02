25ba409f22697ea6b79c9ff560281b5c0b1fc512
Samuel334827 <samuelmoamen200@gmail.com>
2025-04-02 20:55:07 +0200

    solvingerror

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Client
+  }, 
+  {
+    path: 'categorydetails/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
 ];
